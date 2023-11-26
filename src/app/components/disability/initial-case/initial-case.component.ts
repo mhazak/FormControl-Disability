cc32106969ff4c8c16b858fa10fc26b262cca0b6
hazak.marek <hazak.marek@gmail.com>
2023-11-26 02:07:39 +0100

    init - case init

---
+++ src/app/components/disability/initial-case/initial-case.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-initial-case',
+  templateUrl: './initial-case.component.html',
+  styleUrls: ['./initial-case.component.css']
+})
+export class InitialCaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
